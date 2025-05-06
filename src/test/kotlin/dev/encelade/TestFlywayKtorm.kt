4ba7db5fe0b1c5c1050a3609eef1e352666d6679
benckx <benoit.vleminckx@restore.eu>
2025-05-06 09:10:58 +0200

    configure Flyway

---
+++ src/test/kotlin/dev/encelade/TestFlywayKtorm.kt
@@ -0,0 +1,31 @@
+package dev.encelade
+
+import org.flywaydb.core.Flyway
+import org.junit.jupiter.api.Test
+
+class TestFlywayKtorm {
+
+    @Test
+    fun evaluate() {
+        val flyway = Flyway.configure()
+            .dataSource("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1", "sa", "")
+            .locations("classpath:/migrations")
+            .load()
+
+        // log migration info
+        val info = flyway.info()
+        val migrations = info.all()
+
+        println("Classpath: ${System.getProperty("java.class.path")}")
+        println("Flyway Migrations: (${migrations.size})")
+        migrations.forEach { migration ->
+            println("Version: ${migration.version}, Location: ${migration.physicalLocation}, Description: ${migration.description}, State: ${migration.state}")
+        }
+
+        // run migrations
+        flyway.migrate()
+
+        println("Flyway migrations executed successfully!")
+    }
+
+}
---
+++ src/test/resources/migrations/V01__test.sql
@@ -0,0 +1,5 @@
+create table person (
+    id int not null,
+    name varchar(255) not null,
+    primary key (id)
+);
